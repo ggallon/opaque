b8a396f4efc2d1cd3fb003eef26ced4516463d37
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-03-19 13:26:57 +0100

    Remove old comments

--- src/lib.rs
+++ src/lib.rs
@@ -16,19 +16,6 @@ use serde::{Deserialize, Serialize};
 use tsify::Tsify;
 use wasm_bindgen::prelude::*;
 
-// #[wasm_bindgen]
-// extern "C" {
-//     fn alert(s: &str);
-//     #[wasm_bindgen(js_namespace = console)]
-//     fn log(s: &str);
-// }
-
-// macro_rules! console_log {
-//     // Note that this is using the `log` function imported above during
-//     // `bare_bones`
-//     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-// }
-
 enum Error {
     Protocol {
         context: &'static str,
